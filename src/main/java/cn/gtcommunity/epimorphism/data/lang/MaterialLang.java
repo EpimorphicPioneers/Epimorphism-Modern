d85aa06e6593898a275951314659240f21ca0f6a
Gate Guardian <xtzcy76040108@163.com>
2024-02-04 15:57:52 +0800

    MaterialLang Update

--- src/main/java/cn/gtcommunity/epimorphism/data/lang/MaterialLang.java
+++ src/main/java/cn/gtcommunity/epimorphism/data/lang/MaterialLang.java
@@ -1,5 +1,12 @@
 package cn.gtcommunity.epimorphism.data.lang;
 
-public class MaterialLang {
+import net.minecraftforge.common.data.LanguageProvider;
 
+import static cn.gtcommunity.epimorphism.common.data.EPMaterials.*;
+import static cn.gtcommunity.epimorphism.data.lang.EPLangHelper.*;
+
+public class MaterialLang {
+    public static void init(LanguageProvider provider) {
+        addMaterial(provider, GrapheneOxide, "Graphene Oxide", "氧化石墨烯");
+    }
 }
